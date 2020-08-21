68b4d4239bdd8f922b93fcb72b8e76397fb1b129
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-08-21 05:29:13 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,8 +4,14 @@ def turn_count(board)
     if i=="X" || i=="O"
       count+=1
     end
+  end
   return count
 end
 
-def current_player()
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
 end
